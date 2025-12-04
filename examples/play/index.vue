a05e922d5e156a15f57b6afb004d5e67ac71f2b0
刘增伟 <liuzengwei@xianniu.cn>
2025-12-04 18:54:44 +0800

    chore: 更新输入框和按钮样式，调整颜色和边框半径

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div style="margin: 20px;">
-    <el-input v-model="input" placeholder="请输入内容"></el-input>
+    <el-input v-model="input" placeholder="请输入内容" clearable></el-input>
+    <el-button type="info" @click="">按钮</el-button>
+    <el-button  @click="">按钮</el-button>
+    
   </div>
 </template>
 

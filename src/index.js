49bf1536fd1baa377934d33297664057b7b709d2
刘增伟 <liuzengwei@xianniu.cn>
2025-12-22 18:33:37 +0800

    [build] 2.15.5-xn.45

--- src/index.js
+++ src/index.js
@@ -228,7 +228,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '2.15.5-xn.44',
+  version: '2.15.5-xn.45',
   locale: locale.use,
   i18n: locale.i18n,
   install,
